f9aa09bba9e525072c9b8b6e1e9f1df4ca24c3b7
Александр Михайловский <13092004sasha@gmail.com>
2023-05-24 21:15:24 +0300

    Добавьте файлы проекта.

---
+++ task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyString mystr = new MyString("tevirp");
+            Console.WriteLine(mystr.ReverseString());
+        }
+        public class MyString
+        {
+            public string _string = "abc";
+            public MyString(string str)
+            {
+                _string = str;
+            }
+            public string ReverseString()
+            {
+                char[] _char = _string.ToCharArray();
+                Array.Reverse(_char);
+                return new string(_char);
+            }
+
+        }
+    }
+}
